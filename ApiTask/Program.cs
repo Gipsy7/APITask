fbff2f9381da174fae655ae27cc2a8f0ed0ee4dd
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-04-05 18:17:06 -0300

    Creating entity, mapget and mappost

--- ApiTask/Program.cs
+++ ApiTask/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TasksDB"));
 
 var app = builder.Build();
 
@@ -18,4 +22,28 @@ app.MapGet("/", () => "Olá Mundo");
 
 app.MapGet("frases", async () => await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes"));
 
+app.MapGet("/tasks", async (AppDbContext db) => { return await db.Tasks.ToListAsync(); });
+
+app.MapPost("/Tasks", async (Task task, AppDbContext db) => 
+{ 
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
 app.Run();
+
+class Task
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsCompleted { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+    public DbSet<Task> Tasks => Set<Task>();
+
+}
